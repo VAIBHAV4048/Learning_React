e1e444aaee05f9462110016794ec51890d846e75
Vaibhav Pant <vpant4048@gmail.com>
2026-06-22 01:28:04 +0530

    Why JSX is needed

--- app.js
+++ app.js
@@ -2,13 +2,15 @@
         const root=ReactDOM.createRoot(document.getElementById("root"));
         root.render(h1); */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
     React.createElement("h2", {}, "I'm h2 tag"),
   ]),
-);
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);/* How hard it's to read */
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
