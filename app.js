1b3d91ec055281b162556b752516bdb16f4f9b25
Vaibhav Pant <vpant4048@gmail.com>
2026-06-22 01:24:00 +0530

    sibling child element in HTML

--- app.js
+++ app.js
@@ -5,11 +5,10 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
+  React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
-  ),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
 );
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
