2aa360e5cef9f66e077798e52ee6cb10baabcea5
Vaibhav Pant <vpant4048@gmail.com>
2026-06-28 23:48:01 +0530

    Learned how to use JSX inside js started the components learned functional components

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ root.render(parent);*/
 /* this replaces everything inside root */
 
 //JSX->React Element
-const jsxHeading = (
+/* const jsxHeading = (
   <h1 id="heading" className="head">
     Hello i am jsx
   </h1>
-);
+); */
 // this jsx code is transpiled before it reaches js engine - Parcel -SWC4
 // in jsx we have to use camelCase for attributes
 // JSX=> React.createElement =>ReactElement-JS Object=> HTML Element(render)
@@ -35,11 +35,11 @@ const jsxHeading = (
 //Function Based Component-New way
 
 //React Functional Component-> a normal js function which returns some jsx  and jsx is react element
-const HeadingComponent1 = () => (
+/* const HeadingComponent1 = () => (
   <div id="Container">
     <h1 className="head">Here is a functional Component</h1>
   </div>
-);
+); */
 /* const HeadingComponent = () => {
   return <h1 className="heading">Here is a functional Component</h1>;
 }; */
@@ -48,13 +48,51 @@ const HeadingComponent1 = () => (
 //HeadingComponent1 and HeadingComponent both are same
 
 // if i want to render a component inside another component this is known as Component Composition
-const title = () => <h1 className="head">Here is a functional Component</h1>;
+// For composition component that is used inside must have 1st letter capital otherwise jsx treat it as HTML element
+/* const Title = () => <h1 className="head">Here is a functional Component</h1>;
 
 const HeadingComponent1 = () => (
   <div id="Container">
-    <title/>
+    <Title/>
+    <Title><Title/>
+    //line no. 56 and 57 both are same
   </div>
+); */
+// We can also use normal function but we have to return inside it but it's recommended to use arrow function as it looks cleaner
+
+/* const HeadingComponent =function()  {
+  return <h1 className="heading">Here is a functional Component</h1>;
+}; */
+// Amazing super power of JSX we can run any piece JS in between curly braces pair in JSX
+
+/* const Title = () => <h1 className="head">Here is a functional Component</h1>;
+let number=1000;
+const HeadingComponent1 = () => (
+  <div id="Container">
+     <h2>{number}</h2>
+     <h3>{console.log("that's how we use js inside jsx")}</h3>
+   <Title/>
+  </div> 
+); */
+
+// infine loop using component - circular rendering and we can also put a component inside element
+const el=<span>Hello!</span>
+const HeadingComponent1 = () => (
+  <div id="Container">
+     {jsxHeading}
+     <h3>{console.log("that's how we use js inside jsx")}</h3>
+  </div> 
 );
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    Hello i am jsx
+    {el}
+    <HeadingComponent1/>
+  </h1>
+);
+// we can also call react function inside another like{Title()}
+// if we add any  data coming from malicious API JSX will handle it -> means it doesn't blindly execute the code , it prevents cross-site scripting also known as XSS in  real its jsx default escaping that prevents
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
 // How to render functional component
